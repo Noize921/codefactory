9dbc807ca9b4110afa560cacdbe87d0f01ec89e2
Polevoi Vadim <vadimka.polevoi@gmail.com>
2018-11-08 19:27:48 +0200

    Update

--- src/com/codefactory/internship/lesson6/app/ATMApp.java
+++ src/com/codefactory/internship/lesson6/app/ATMApp.java
@@ -6,12 +6,30 @@ import javax.swing.*;
 
 public class ATMApp {
     public static void main(String[] args) {
+        int numberOfTries = 0;
+
         JOptionPane.showMessageDialog(null, "Welcome to ATM!");
 
-        String userInput = JOptionPane.showInputDialog("Enter your PIN:");
-        final int PIN = Integer.parseInt(userInput);
-        boolean result = PinUtils.validatePin(PIN);
+        boolean result = false;
 
-        JOptionPane.showMessageDialog(null, (result) ? "Correct PIN" : "Wrong PIN");
+        while (true) {
+            String userInput = JOptionPane.showInputDialog("Enter your PIN:");
+            final int PIN = Integer.parseInt(userInput);
+            result = PinUtils.validatePin(PIN);
+
+            if (result == false) {
+                if (numberOfTries == 2) {
+                    showErrorMessage();
+                    break;
+                }
+
+                numberOfTries++;
+                JOptionPane.showMessageDialog(null, "Sorry, wrong PIN!\nTries left: " + (3 - numberOfTries));
+            }
+        }
+    }
+
+    private static void showErrorMessage() {
+        JOptionPane.showMessageDialog(null, "No more attempts left!", "Error message", JOptionPane.ERROR_MESSAGE);
     }
 }
